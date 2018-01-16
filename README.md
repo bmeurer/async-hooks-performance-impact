5d179f18adad02e4be0e89afcddaefe038ce901b
Benedikt Meurer <bmeurer@google.com>
2018-01-16 13:11:02 +0100

    Initial import of the benchmark and the results.
    This repository contains the data to address the question of the Promise
    performance impact of async_hooks raised in
    https://github.com/nodejs/benchmarking/issues/188.

---
+++ README.md
@@ -0,0 +1,48 @@
+# Performance impact of async_hooks
+
+This is a simple demo of the potential performance impact of `async_hooks`
+on Promise heavy workloads. This repository contains the popular
+[Bluebird](https://github.com/petkaantonov/bluebird) benchmarks and the
+so-called *Wikipedia Promise* benchmark with and
+without `async_hooks` (running with native promises).
+
+To measure the actual impact there's a version of each benchmark with
+an empty `init` hook, i.e. we prepend the following code to the actual
+code under test:
+
+```js
+const async_hooks = require('async_hooks');
+const hook = async_hooks.createHook({
+    init(asyncId, type, triggerAsyncId) { }
+});
+hook.enable();
+```
+
+
+## Results
+
+Here the results of running the benchmarks with and without `async_hooks`
+enabled:
+
+| Benchmark                      | Node 8.4.0 | Node 9.3.0 |
+| ------------------------------:| ----------:| ----------:|
+| Bluebird-doxbee (asynchooks)   |     458 ms |     369 ms |
+| Bluebird-doxbee (regular)      |     301 ms |     179 ms |
+| Bluebird-parallel (asynchooks) |    1310 ms |    1079 ms |
+| Bluebird-parallel (regular)    |     839 ms |     671 ms |
+| Wikipedia (asynchooks)         |    1656 ms |    1790 ms |
+| Wikipedia (regular)            |     930 ms |     863 ms |
+
+
+## Bluebird benchmarks
+
+The files here are modified from: [https://github.com/petkaantonov/bluebird/tree/master/benchmark](https://github.com/petkaantonov/bluebird/tree/master/benchmark)
+
+The current version is based on `bluebird@c0d4472cecd523c2f9d4805a23d87be3cfe03b41`
+
+
+## Wikipedia Benchmark
+
+The files here are modified from: [https://github.com/wikimedia/web-stream-util](https://github.com/wikimedia/web-stream-util)
+
+The current version is based on `web-stream-util@fc76740cd6a73dcb044251a233bc3c868d3c9a77`
---
+++ bluebird-parallel-regular.js
@@ -0,0 +1,153 @@
+var global = {};
+
+var lifter = function(nodefn) {
+  return function() {
+    var self = this;
+    var l = arguments.length;
+    var args = new Array(l + 1);
+    for (var i = 0; i < l; ++i) {
+      args[i] = arguments[i];
+    }
+    return new Promise(function(resolve, reject) {
+      args[l] = function(err, val) {
+        if (err) reject(err);
+        else resolve(val);
+      };
+      nodefn.apply(self, args);
+    });
+  };
+};
+
+function dummy(n) {
+  return function dummy_n() {
+    var cb = arguments[n - 1];
+    cb();
+  }
+}
+
+function fakemaker(dummy, wrap) {
+
+  var dummy_2 = dummy(2),
+  dummy_1 = dummy(1);
+
+  // a queryish object with all
+  // kinds of functions
+  function queryish() {
+    return {
+      execWithin: dummy_2,
+      exec: dummy_1,
+      get: dummy_1,
+      all: dummy_1,
+    };
+  }
+
+  global.uuid = { v1: function v1() {} };
+
+  global.userAccount = { };
+
+  global.account = { };
+
+  global.blobManager = {
+    create: function create() {
+      return {
+        put: dummy_2,
+      }
+    }
+  };
+
+  var cqQueryish = queryish();
+
+  global.self = {
+    byUuidOrPath: queryish,
+    createQuery: wrap(function createQuery(x, y, cb, ctx) {
+      cb.call(ctx, null, cqQueryish);
+    })
+  };
+
+  global.File = {
+    insert: queryish,
+    whereUpdate: queryish
+  };
+
+  global.FileVersion = {
+    insert: queryish
+  };
+
+  global.Version = {
+    createHash: function createHash(v) { return 1; },
+    insert: queryish
+  };
+
+  global.db = {
+    begin: function begin() {
+      return {
+        commit: dummy_1,
+        rollback: dummy_1,
+      };
+    }
+  };
+}
+
+function dummyP(n) {
+  return lifter(dummy(n));
+}
+
+function upload(stream, idOrPath, tag, done) {
+  var queries = new Array(global.parallelQueries);
+  var tx = global.db.begin();
+
+  for (var i = 0, len = queries.length; i < len; ++i) {
+    queries[i] = global.FileVersion.insert({index: i}).execWithin(tx);
+  }
+
+  Promise.all(queries).then(function() {
+    tx.commit();
+    done();
+  }, function(err) {
+    tx.rollback();
+    done(err);
+  });
+}
+
+function perf() {
+  var errs = 0;
+  var lastErr;
+
+  var fn = upload;
+  var start = Date.now();
+  var warmedUp = 0;
+
+  var n = 10000;
+  var times = n;
+  var tot = Math.min(350, times);
+
+  global.parallelQueries = 25;
+
+  for (var k = 0, kn = tot; k < kn; ++k)
+  fn(k, 'b', 'c', warmup);
+
+  var memMax; var memStart; var start;
+  function warmup() {
+    warmedUp++
+    if (warmedUp === tot) {
+      start = Date.now();
+
+      for (var k = 0, kn = n; k < kn; ++k)
+        fn(k, 'b', 'c', cb);
+    }
+  }
+
+  function cb(err) {
+    if (err && err.message !== 'intentional failure') {
+      ++errs;
+      lastErr = err;
+    }
+    if (!--times) {
+      fn.end && fn.end();
+      console.log(`Bluebird-parallel (regular): ${Date.now() - start} ms.`);
+    }
+  }
+}
+
+fakemaker(dummyP, lifter);
+perf();
